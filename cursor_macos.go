73a4a7cdac5814408c7e3ddfd9fea17542f6679a
Jason Cameron <git@jasoncameron.dev>
2025-01-31 20:03:51 -0500

    fix(macos): Correctly get current display index (main)
    The previous implementation used `[NSEvent mouseLocation]` and
    `[NSScreen screens]` which are deprecated and don't work well
    with multiple displays. It also had issues when displays had
    overlapping frames.
    
    This commit replaces it with `CGEventGetLocation`,
    `CGGetActiveDisplayList` and `CGDisplayBounds` which are the
    recommended way to get the current display index on macOS.  This
    fixes the issue where the incorrect display index was returned
    when using multiple displays.

--- cursor_macos.go
+++ cursor_macos.go
@@ -3,27 +3,38 @@
 package displayindex
 
 /*
-#cgo darwin LDFLAGS: -framework Cocoa
-#import <Cocoa/Cocoa.h>
+#cgo darwin LDFLAGS: -framework CoreGraphics -framework Foundation
+#include <CoreGraphics/CoreGraphics.h>
 
-int getDisplayIndex() {
-    NSPoint mouseLoc = [NSEvent mouseLocation];
-    NSArray *screens = [NSScreen screens];
+int getCurrentDisplayIndex() {
+    CGEventRef event = CGEventCreate(nil);
+    CGPoint location = CGEventGetLocation(event);
+    CFRelease(event);
 
-    for (NSUInteger i = 0; i < [screens count]; i++) {
-        NSRect frame = [[screens objectAtIndex:i] frame];
-        if (NSMouseInRect(mouseLoc, frame, NO)) {
+    CGDirectDisplayID displays[16];
+    uint32_t displayCount = 0;
+
+    CGError err = CGGetActiveDisplayList(16, displays, &displayCount);
+    if (err != kCGErrorSuccess || displayCount == 0) {
+        return -1;
+    }
+
+    for (uint32_t i = 0; i < displayCount; i++) {
+        CGRect bounds = CGDisplayBounds(displays[i]);
+        if (CGRectContainsPoint(bounds, location)) {
             return (int)i;
         }
     }
+
     return -1;
 }
 */
 import "C"
+
 import "fmt"
 
 func CurrentDisplayIndex() (int, error) {
-	index := C.getDisplayIndex()
+	index := C.getCurrentDisplayIndex()
 	if index == -1 {
 		return -1, fmt.Errorf("cursor not on any active display")
 	}
